b720846c55f515aaeebcdb3bab00ca549b0f5711
Juan Ramirez <juanesramirezgalan@gmail.com>
2026-02-14 15:12:04 -0500

    Operadores

---
+++ arithmetic_operators/arithmetic_operators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int = 15
+	var b int = 10
+
+	fmt.Println("Addition:", a + b)
+	fmt.Println("Substraction:", a - b)
+	fmt.Println("Multiplication:", a * b)
+	fmt.Println("Division:", a / b)
+	fmt.Println("Modulo:", a % b)
+}
